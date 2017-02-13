77879c1198a7cbd08ec92739855954635bc02566
js296 <js296@njit.edu>
2017-02-13 00:24:11 -0500

    validates interest rate to be less than or equal to 15

--- display_results.php
+++ display_results.php
@@ -17,8 +17,12 @@
         $error_message = 'Interest rate must be a valid number.'; 
     } else if ( $interest_rate <= 0 ) {
         $error_message = 'Interest rate must be greater than zero.'; 
+    }
+      else if (!($interest_rate <=  15)) {
+      	$error_message = 'Interest rate must be less than or equal to 15';
+    }
     // validate years
-    } else if ( $years === FALSE ) {
+    else if ( $years === FALSE ) {
         $error_message = 'Years must be a valid whole number.';
     } else if ( $years <= 0 ) {
         $error_message = 'Years must be greater than zero.';
